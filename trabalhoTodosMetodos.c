cc1d6293711039161b952b963ebef104e53ecfd9
alexzarp <alexszds@gmail.com>
2020-10-07 13:25:43 -0300

    certo

--- trabalhoTodosMetodos.c
+++ trabalhoTodosMetodos.c
@@ -4,30 +4,33 @@
 #include <time.h>
 
 void bubble_sort(int *vet, int n){
-    int comparacoes = 0, trocas = 0;
+    long int comparacoes = 0, trocas = 0;
+    int troca, flag;
     for(int i = n-1; i>=1; i--){
-        int flag = 0;
+        flag = 0;
         for(int j = 0; j<=i-1; j++){
+             comparacoes+=1;
             if (vet[j] > vet[j+1]){  
-                int troca = vet[j];
+                troca = vet[j];
                 vet[j] = vet[j+1];
                 vet[j+1] = troca;
                 flag = 1;
                 trocas+=1;
             }
-            comparacoes+=1;
+           
         }
         if (flag == 0){
                 break;
         }
         
     }
-    printf("Comparacoes: %d\n", comparacoes);
-    printf("Trocas: %d\n", trocas);
+    printf("Comparacoes: %ld\n", comparacoes);
+    printf("Trocas: %ld\n", trocas);
 }
 
 int insertionsort(int *v, int n) { 
-    int i, c, j, comparacoes = 0, trocas = 0; 
+    int i, c, j;
+    long int comparacoes = 0, trocas = 0; 
     for (int i = 0; i < n; i++){
         int c = v[i];
         int j = i - 1;
@@ -45,29 +48,30 @@ int insertionsort(int *v, int n) {
         v[j + 1] = c;
     }
     
-    printf("Comparacoes: %d\n", comparacoes);
-    printf("Trocas: %d\n", trocas);
+    printf("Comparacoes: %ld\n", comparacoes);
+    printf("Trocas: %ld\n", trocas);
 }
 void selectionsort(int *v, int n){
-	int menor, trocas=0, comp=0;
+ int menor;
+    long int trocas=0, comp=0;
 
-	for(int i = 0; i<=n-2; i++){
+ for(int i = 0; i<=n-2; i++){
         menor = i;
         for(int j = i+1; j <= n-1; j++){
-			comp+=1;
+   comp+=1;
             if(v[menor] > v[j]){
                 menor = j;
-			}
-		} 
-		if (menor != i){
-			int troca = v[i];
-			v[i] = v[menor];
-			v[menor] = troca; 
-			trocas+=1;
-		}
+   }
+  } 
+  if (menor != i){
+   int troca = v[i];
+   v[i] = v[menor];
+   v[menor] = troca; 
+   trocas+=1;
+  }
 }
-printf("Comparacoes: %d\n", comp);
-printf("Trocas: %d\n", trocas);
+printf("Comparacoes: %ld\n", comp);
+printf("Trocas: %ld\n", trocas);
 
 }
 void geraVetorCrescente (int *vet , int n) {
@@ -146,13 +150,13 @@ void main(){
     end = clock();
 
     printf("\nValore ordenados ");
-    for(int q=0;q<n;q++){
+   /* for(int q=0;q<n;q++){
         printf("%d ",vet[q]);
         }
-    printf("\n");
+    printf("\n");*/
 
     difTempo = ((double)end - start)/CLOCKS_PER_SEC;
     
-    printf("\nTempo: %lf\n",difTempo);
-    printf("Tempo em milesegundos: %lf\n",difTempo*1000);
+    printf("\nTempo(s): %lf\n",difTempo);
+    printf("Tempo(ms) %lf\n",difTempo*1000);
 }
