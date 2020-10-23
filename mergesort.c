4fbf260124a1d52f8c0ffcaa0047e357053c5048
alexzarp <alexszds@gmail.com>
2020-10-22 22:54:03 -0300

    Mege sort ainda todo cagado

--- mergesort.c
+++ mergesort.c
@@ -2,13 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 
-void swap(int x, int y){ 
-   int temp;
-   temp = x;
-   x = y;
-   y = temp;
+void mergesort (int *vet, int inicio, int fim) {
+    if (inicio < fim) {
+        int meio = intercala((inicio + fim)/2);
+        mergesort(vet, inicio, meio);
+        mergesort(vet, meio+1, fim);
+        intercala(vet, inicio, meio, fim);
+    }
 }
 
+void intercala (int *vet, int inicio, int meio, int fim) {
+    int aux = malloc (fim - inicio + 1);
+    int i = inicio;
+    int j = meio + 1;
+    int *k = 0;
+
+    while (i <= meio && j <= fim) {
+        if (vet[i] <= vet[j]) {
+            aux[k] = vet[i];
+            i++;
+        } else {
+            aux[k] = vet[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i <= meio) {
+        aux[k] = vet[i];
+        k++;
+        i++;
+    }
+
+    while (j <= fim) {
+        aux[k] = vet[j];
+        k++;
+        j++;
+    }
+
+    for (k = inicio; k <= fim; k++) {
+        vet[*k] = aux[k - inicio];
+    }
+    free(aux);
+}
 
 void main(){
     int vet[5] = {3,27,6,4,5};
@@ -20,7 +56,7 @@ void main(){
         }
     printf("\n");
 
-    quicksort(vet, 0, n);
+    mergesort(vet, 0, n);
 
     printf("Valore ordenados ");
     for(int q=0;q<n;q++){
