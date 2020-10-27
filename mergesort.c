7ffa5e5dcc63c7fa9202a0f03617f6962372fbeb
alexzarp <alexszds@gmail.com>
2020-10-27 15:26:31 -0300

    +

--- mergesort.c
+++ mergesort.c
@@ -1,54 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
-void mergesort (int *vet, int inicio, int fim) {
-    if (inicio < fim) {
-        int meio = intercala((inicio + fim)/2);
-        mergesort(vet, inicio, meio);
-        mergesort(vet, meio+1, fim);
-        intercala(vet, inicio, meio, fim);
-    }
-}
+void mergesort (int *vet, int inicio, int fim);
+void intercala (int *vet, int inicio, int meio, int fim);
 
-void intercala (int *vet, int inicio, int meio, int fim) {
-    int aux = malloc (fim - inicio + 1);
-    int i = inicio;
-    int j = meio + 1;
-    int *k = 0;
-
-    while (i <= meio && j <= fim) {
-        if (vet[i] <= vet[j]) {
-            aux[k] = vet[i];
-            i++;
-        } else {
-            aux[k] = vet[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i <= meio) {
-        aux[k] = vet[i];
-        k++;
-        i++;
-    }
-
-    while (j <= fim) {
-        aux[k] = vet[j];
-        k++;
-        j++;
-    }
-
-    for (k = inicio; k <= fim; k++) {
-        vet[*k] = aux[k - inicio];
-    }
-    free(aux);
-}
 
 void main(){
-    int vet[5] = {3,27,6,4,5};
-    int n=5;
+    int vet[8] = {5,1,9,50,-6,100,7,50};
+    int n=8;
 
     printf("Valore desordenados ");
     for(int q=0;q<n;q++){
@@ -63,4 +24,50 @@ void main(){
         printf("%d ",vet[q]);
         }
     printf("\n");
+}
+
+void mergesort (int *vet, int inicio, int fim) {
+    if (inicio < fim) {
+        int meio =floor(((inicio + fim)/2));
+        mergesort(vet, inicio, meio);
+        mergesort(vet, meio+1, fim);
+        intercala(vet, inicio, meio, fim);
+    }
+}
+void intercala (int *vet, int inicio, int meio, int fim) {
+    int aux = fim - inicio + 1;
+    int i = inicio;
+    int j = meio + 1;
+    int p1 = 0, p2 = 0;
+    int *k;
+    k = (int ) malloc(auxsizeof(int));
+
+    if(k != NULL){
+        for(int x = 0; x<aux; x++){
+            if (!p1 && !p2) {
+                if(vet[i] < vet[j]){
+                    k[x] = vet[i++];
+                }
+                else{
+                    k[x] = vet[j++];
+                }
+                if(i>meio) p1=1;
+                if(j>fim) p2=1;
+            }
+            else{
+                if(!p1){
+                    k[x] = vet[i++];
+                }else{
+                    k[x] = vet[j++];
+                }
+            }
+
+
+        }
+
+for (int aux = inicio; aux <= fim; aux++) {
+        vet[aux] = k[aux - inicio];
+}
+}
+free(k);
 }
