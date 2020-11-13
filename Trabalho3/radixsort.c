c05339f53f9717a1c9cc753798aa9a4d0ea898c7
alexzarp <alexszds@gmail.com>
2020-11-12 22:59:01 -0300

    secret

--- Trabalho3/radixsort.c
+++ Trabalho3/radixsort.c
@@ -1,6 +1,6 @@
 #include "getMax.c"
-void countSort(int *vet, int n, int exp) { 
-    int output[n];
+void countingsort(int *vet, int n, int exp) { 
+    int saida[n];
     int i, cont[10] = {0}; //Todas as posições incialmente como 0
   
     
@@ -12,12 +12,12 @@ void countSort(int *vet, int n, int exp) {
   
     
     for (i = n - 1; i >= 0; i--) { 
-        output[cont[(vet[i] / exp) % 10] - 1] = vet[i]; 
+        saida[cont[(vet[i] / exp) % 10] - 1] = vet[i]; 
         cont[(vet[i] / exp) % 10]--; 
     }
 
     for (i = 0; i < n; i++) 
-        vet[i] = output[i]; 
+        vet[i] = saida[i]; 
 } 
 
 void radixsort(int *vet, int n) { 
@@ -25,5 +25,5 @@ void radixsort(int *vet, int n) {
     int m = getMax(vet, n); 
 
     for (int exp = 1; m / exp > 0; exp *= 10) 
-        countSort(vet, n, exp); 
+        countingsort(vet, n, exp); 
 } 
