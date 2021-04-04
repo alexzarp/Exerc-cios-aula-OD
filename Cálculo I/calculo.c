cc56d13479bc384909fb57d0e38962afd3df6993
Alex Sandro <alexszds@gmail.com>
2021-04-03 22:55:08 -0300

    Programa final eu acho

--- Cálculo I/calculo.c
+++ Cálculo I/calculo.c
@@ -10,17 +10,35 @@ double func(int x) {
 
 int main() {
     //double e = 2.71828;
-    int x = 0; int x2 = 0;
-    scanf("%d%d", &x, &x2);
+    double x = 0; double x2 = 0; double erro = 0.1;
+    printf("Insira o intervalo no formato x y(somente com espaço entre os números e nada além disso)\n");
+    scanf("%lf%lf", &x, &x2);
 
     double p1 = func(x); double p2 = func(x2);
-    if ((p1 >= 0 && p2 < 0) || (p1 < 0 && p2 >= 0)) {
-        printf("a equação tem pelo menos uma solução neste intervalo\n");
+    if (x > x2) {
+        double aux = p1;
+        p1 = p2;
+        p2 = aux;
 
-        if (x <= 0)
-            printf("intervalo [%d,%d]\n", x, x2);
-        else 
-            printf("intervalo [%d,%d]\n", x2, x);
+        aux = x;
+        x = x2;
+        x2 = aux;
+    }
+
+    if ((p1 > 0 && p2 < 0) || (p1 < 0 && p2 > 0)) {
+        printf("a equação tem pelo menos uma solução neste intervalo");
+
+        while (fabs(x - x2) > erro) {
+            double centro = x + ((x2 - x) / 2);
+
+            if ((func(x) > 0 && func(centro) > 0) || (func(x) < 0 && func(centro) < 0))
+                x = centro;
+            else
+                x2 = centro;
+        }
+        
+        printf(" [%lf,%lf]\n", x, x2);
+                
     } else
         printf("não é possível afirmar que existe solução neste intervalo, tente outros dois números\n");
 
